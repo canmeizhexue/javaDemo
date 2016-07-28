ff2dd4b1964b33a890e4b2a834de1b253fc3aa64
zengyaping <zengyaping@htmimi.com>
2016-07-28 16:49:35 +0800

    weakHashMap

---
+++ src/com/canmeizhexue/javademo/threads/WeakMapTest.java
@@ -0,0 +1,54 @@
+package com.canmeizhexue.javademo.threads;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.WeakHashMap;
+
+public class WeakMapTest {
+	/**
+	 * 当键不再被引用而被回收后，这个map中会将对应的条目删除
+	 * @param args
+	 */
+	public static void main(String[]args){
+		Map map = new WeakHashMap();
+		//因为很快MyKey对象没被其他地方引用
+		map.put(new MyKey("helloworld"), "12345");
+		//字符串类型的key却不会被回收
+		map.put("wekmap", "map test");
+		printMap(map);
+		for(int i=0;i<10000;i++){
+			System.gc();
+/*			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}*/
+			System.out.println("current loop is  "+i);
+			printMap(map);
+		}
+		System.out.println("test end ");
+	}
+	public static void printMap(Map map){
+		Set set=map.keySet();
+		Iterator iterator=set.iterator();
+		while(iterator.hasNext()){
+			Object key = iterator.next();
+			System.out.println("key is "+key+"   value is "+map.get(key));
+		}
+	}
+	public static class MyKey{
+		String key;
+		
+		public MyKey(String key) {
+			super();
+			this.key = key;
+		}
+
+		public String toString() {
+			 
+			return key;
+		}
+	}
+}
